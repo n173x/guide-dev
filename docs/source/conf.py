5b5f841d7fb4f2ac1dbfda71f9faca8cee801943
n173x <112439924+n173x@users.noreply.github.com>
2022-10-11 23:27:15 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Mirai! Guide'
+# copyright = '2021, Graziella'
+author = 'Nitex'
 
 release = '0.1'
 version = '0.1.0'
